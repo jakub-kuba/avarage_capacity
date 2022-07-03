7d72ea7a79e4a44e6824e2166b2e2359e884ec1c
jakub-kuba <jakub.kuba.piech@gmail.com>
2022-07-04 00:09:23 +0200

    Adding the necessary files

--- avarage_capacity.py
+++ avarage_capacity.py
@@ -1,27 +1,35 @@
+# Author: jakub-kuba
+
+# Select 5 countries and see the average stadium capacity
+# in their top football league (source:stadiony.net)
+
 import pandas as pd
 import pickle
 import sys
 from datetime import datetime
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-#check if the file exists
-def open_file(file_name, mode):     
+def open_file(file_name, mode):
+    """Check if the correct file exists."""
     try:
         f = open(file_name, mode)
         f.close()
-    except:
-        print("The file not found. Ending the program.\n")
+    except FileNotFoundError:
+        print("\nThe file not found. Ending the program.\n")
         sys.exit()
 
-#create a DataFrame
 def create_df():
+    """Create an empty DataFrame."""
     column_names = ["League", "Capacity"]
     new_df = pd.DataFrame(columns = column_names)
     new_df['Capacity'] = new_df['Capacity'].astype('int32')
     return new_df
 
-#load the file and show the names of the countries in alphabetical order
-def get_countries():
+def get_countries(myfile):
+    """Load the file and show the names of the countries
+    in alphabetical order.
+    """
     f = open(myfile, "rb")
     mydict = pickle.load(f)
     keys = sorted(list(mydict.keys()))
@@ -30,8 +38,8 @@ def get_countries():
     f.close()
     return countries
 
-#choose a country from the list
-def select_country(number):
+def select_country(countries, number):
+    """Choose a country from the list."""
     choice = None
     numb = str(number)
     text = "What is country " + numb + "? "  
@@ -39,59 +47,65 @@ def select_country(number):
         choice = input(text)
     return choice
 
-#return the address of the selected country
-def get_address():
+def get_address(countries, selected):
+    """Return the address of the selected country."""
     address = countries.get(selected)
     cou = pd.read_html(address)
     return cou
 
-#determine which table relates to the best league
 def league_level(country):
+    """Determine which table relates to the best league"""
     if len(country[0]) < 3:
         std = country[1]
     else:
         std = country[0]
     return std
 
-#modify the DF and calculate the avarage for a given division
 def avg_capacity(df):
+    """Modify the selected DF and calculate the avarage for a given league"""
     df.columns=['Name', 'City', 'Club', 'Capacity']
     df['Capacity'] = df['Capacity'].str.replace(" ","")
     df['Capacity'] = df['Capacity'].astype('int64')
-    cap = round(division['Capacity'].mean())
+    cap = round(df['Capacity'].mean())
     return cap
 
+def main():
+    myfile = "countries.dat"
+    open_file(myfile, "rb")
+    new_df = create_df()
+    countries = get_countries(myfile)
+    number = 1
 
-myfile = ("countries.dat")
-start = open_file(myfile, "rb")
-new_df = create_df()
-countries = get_countries()
-number = 1
+    for n in range(5):
+        selected = select_country(countries, number)
+        myaddress = get_address(countries, selected)
+        del countries[selected]
+        division = league_level(myaddress)
+        capacity = avg_capacity(division)   
+        dic = {'League': selected, 'Capacity': capacity}
+        new_df = new_df.append(dic, ignore_index = True)
+        number += 1
 
-for n in range(5):
-    selected = select_country(number)
-    myaddress = get_address()
-    del countries[selected]
-    division = league_level(myaddress)
-    capacity = avg_capacity(division)   
-    dic = {'League': selected, 'Capacity': capacity}
-    new_df = new_df.append(dic, ignore_index = True)
-    number += 1
+    new_df = new_df.sort_values(by=['Capacity'],
+            ascending=False).reset_index(drop=True)
 
-new_df = new_df.sort_values(by=['Capacity'],
-         ascending=False).reset_index(drop=True)
+    now = datetime.now()
+    date = now.strftime('%d-%b-%Y_%H-%M')
+    new_df.to_excel("files/avarage capacity "+date+".xlsx", index=False)
 
-now = datetime.now()
-date = now.strftime('%d-%b-%Y_%H-%M')
-new_df.to_excel("files/avarage capacity "+date+".xlsx", index=False)
+    print("\nThis is a result:\n", new_df)
 
-print("\nThis is a result:\n", new_df)
+    fig,ax = plt.subplots(figsize=(9, 4.5))
+    sns.set(style="whitegrid")
+    ax.set_axisbelow(True)
+    ax.grid(color='gray')
+    ax.set_title("Average stadium capacity in the top football leagues")
+    x = "Capacity"
+    y = "League"
+    colours = ["#07941c", "#8c8989", "#8c8989", "#8c8989", "#8c8989"]
+    customPalette = sns.set_palette(sns.color_palette(colours))
+    sns.barplot(x,y, data=new_df, orient='h', palette=customPalette)
+    plt.show()
 
-
-
-
-
-
-
-
-
+if __name__== "__main__":
+      main()
---
+++ countries_editor.py
@@ -0,0 +1,75 @@
+# Author: jakub-kuba
+
+# The program allows you to edit a file containing countries
+# and addresses (stadiony.net)
+
+import pickle
+import sys
+
+def open_file(file_name, mode):
+    """Check if the correct file exists"""
+    try:
+        f = open(file_name, mode)
+        f.close()
+    except FileNotFoundError:
+        print("\nThe file not found. Ending the program.\n")
+        sys.exit()
+
+def ask_yes_no(myfile, question):
+    """Add a country & address to the dictionary"""
+    f = open(myfile, "rb")
+    mydict = pickle.load(f)
+    keys_sorted = sorted(mydict.keys())
+    dict_sorted = {key:mydict[key] for key in keys_sorted}
+    print("Countries available:\n", dict_sorted)
+    f.close()
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+        while response == "y":
+            f = open(myfile, "rb")
+            mydict = pickle.load(f)
+            f.close()
+            country = input("Add a country: ")
+            add = input("Address: ")
+            mydict[country] = add
+            f = open(myfile, "wb")
+            pickle.dump(mydict, f)
+            keys = sorted(list(mydict.keys()))
+            print("Countries available:", keys)
+            f.close()
+            response = input(question).lower()
+
+def delete_country(myfile, question):
+    """Delete a country from the dictionary"""
+    f = open(myfile, "rb")
+    mydict = pickle.load(f)
+    keys_sorted = sorted(mydict.keys())
+    dict_sorted = {key:mydict[key] for key in keys_sorted}
+    print("Countries available:\n", dict_sorted)
+    f.close()
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+        while response == "y":
+            f = open(myfile, "rb")
+            mydict = pickle.load(f)
+            f.close()
+            country = input("Delete a country: ")
+            while country in mydict:
+                del mydict[country]
+                f = open(myfile, "wb")
+                pickle.dump(mydict, f)
+                keys = sorted(list(mydict.keys()))
+                print("Countries available:", keys)
+                f.close()
+                response = input(question).lower()
+
+def main():
+    myfile = "countries.dat"
+    open_file(myfile, "rb")
+    ask_yes_no(myfile, "Do you want to add a country? (y/n): ")
+    delete_country(myfile, "Do you want to delete a country? (y/n): ")
+  
+if __name__== "__main__":
+      main()
